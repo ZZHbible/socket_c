39dc5846d27d583ce36f65abb84f8b0261504137
ZZHbible <804459116@qq.com>
2022-11-11 14:30:23 +0800

    server.c

--- server.c
+++ server.c
@@ -61,6 +61,8 @@ int main()
     {
         if (ReadToSend)
         {
+            ReadToSend=0;
+
             // 判断线程存活数量
             int i;
             for (i = 0; i < thrReceiveClientCount; i++)
@@ -81,30 +83,72 @@ int main()
             else
             {
                 // printf("conClientCount = %d\n", conClientCount);
-                char **ans = _str_split(buffer, ' ');
+                // 把这部分逻辑写入 mysocket.c
+                char copy_buf_send[BUFSIZ];
+                memcpy(copy_buf_send, buffer, sizeof(buffer) / sizeof(buffer[0]) * sizeof(char));
+                char **ans = _str_split(copy_buf_send, ' ');
                 target_Client = atoi(ans[0]);
-
-                for (i = 0; i < conClientCount; i++)
+                if (target_Client == -1)
                 {
-                    if (arrConSocket[i].socketCon == send_Client)
+                    char file_signal[BUFSIZ] = "-1 ";
+                    strcat(file_signal, ans[2]);
+                    // printf("file_signal:%s\n",file_signal);
+                    send(send_Client, file_signal, strlen(file_signal), 0);
+                    // printf("open file:%s\n",ans[1]);
+                    FILE *fp = fopen(ans[1], "r");
+                    bzero(buffer, BUFSIZ);
+                    if (NULL == fp)
                     {
-                        ReadToSend = 0;
-                        continue;
+                        printf("File:%s Not Found\n", ans[1]);
                     }
-                    if (arrConSocket[i].socketCon == target_Client)
+                    else
                     {
-                        printf("socketCon = %d\nbuffer is: %s\n", arrConSocket[i].socketCon, buffer);
-                        int sendMsg_len = send(arrConSocket[i].socketCon, ans[1], strlen(ans[1]), 0);
-                        if (sendMsg_len > 0)
+
+                        int length = 0;
+                        // 每读取一段数据，便将其发送给客户端，循环直到文件读完为止
+                        while ((length = fread(buffer, sizeof(char), BUFSIZ, fp)) > 0)
                         {
-                            printf("Send Message to %s:%d successful\n", arrConSocket[i].ipaddr, arrConSocket[i].port);
+
+                            buffer[length] = '\0';
+                            printf("buffer:%s\n",buffer);
+                            if (send(send_Client, buffer, length, 0) < 0)
+                            {
+                                printf("Send File:%s Failed./n", ans[1]);
+                                break;
+                            }
+                            bzero(buffer, BUFSIZ);
+                        }
+                        // 关闭文件
+                        fclose(fp);
+
+                    }
+                    // send(send_Client,"end...",7,0);
+
+                }
+                if (target_Client > 0)
+                {
+                    for (i = 0; i < conClientCount; i++)
+                    {
+                        if (arrConSocket[i].socketCon == send_Client)
+                        {
                             ReadToSend = 0;
+                            continue;
                         }
-                        else
+                        if (arrConSocket[i].socketCon == target_Client)
                         {
-                            printf("Fail to send message to %s:%d\n", arrConSocket[i].ipaddr, arrConSocket[i].port);
+                            printf("socketCon = %d\nbuffer is: %s\n", arrConSocket[i].socketCon, buffer);
+                            int sendMsg_len = send(arrConSocket[i].socketCon, ans[1], strlen(ans[1]), 0);
+                            if (sendMsg_len > 0)
+                            {
+                                printf("Send Message to %s:%d successful\n", arrConSocket[i].ipaddr, arrConSocket[i].port);
+                                ReadToSend = 0;
+                            }
+                            else
+                            {
+                                printf("Fail to send message to %s:%d\n", arrConSocket[i].ipaddr, arrConSocket[i].port);
+                            }
+                            break;
                         }
-                        break;
                     }
                 }
             }
@@ -120,208 +164,3 @@ int main()
 
     return 0;
 }
-
-/********************************************************************
- *   Function Name: void *fun_thrAcceptHandler(void *socketListen)
- *   Description: 监听客户端的连接请求，获取待连接客户端的网络信息，并为该客户端创建子线程.
- *   Called By: server.c[main]
- *   Input: socketListen -> 表示用于监听的被动套接字
- *********************************************************************/
-void *fun_thrAcceptHandler(void *socketListen)
-{
-    while (1)
-    {
-        int sockaddr_in_size = sizeof(struct sockaddr_in);
-        struct sockaddr_in client_addr;
-        int _socketListen = *((int *)socketListen);
-
-        /* 接收相应的客户端的连接请求 */
-        int socketCon = accept(_socketListen, (struct sockaddr *)(&client_addr), (socklen_t *)(&sockaddr_in_size));
-        if (socketCon < 0)
-        {
-            printf("call accept()");
-        }
-        else
-        {
-            printf("Connected %s:%d\n", inet_ntoa(client_addr.sin_addr), client_addr.sin_port);
-        }
-        printf("Client socket: %d\n", socketCon);
-
-        /* 获取新客户端的网络信息 */
-        _MySocketInfo socketInfo;
-        socketInfo.socketCon = socketCon;
-        socketInfo.ipaddr = inet_ntoa(client_addr.sin_addr);
-        socketInfo.port = client_addr.sin_port;
-
-        /* 将新客户端的网络信息保存在 arrConSocket 数组中 */
-        arrConSocket[conClientCount++] = socketInfo;
-        // conClientCount++;
-        printf("Number of users: %d\n", conClientCount);
-
-        /* 为新连接的客户端开辟线程 fun_thrReceiveHandler，该线程用来循环接收客户端的数据 */
-        pthread_t thrReceive = 0;
-        pthread_create(&thrReceive, NULL, fun_thrReceiveHandler, &socketInfo);
-        arrThrReceiveClient[thrReceiveClientCount++] = thrReceive;
-        // thrReceiveClientCount ++;
-        printf("A thread has been created for the user.\n");
-
-        /* 让进程休息0.1秒 */
-        usleep(100000);
-    }
-
-    char *s = "Safe exit from the receive process ...";
-    pthread_exit(s);
-}
-
-/********************************************************************
- *   Function Name: void *fun_thrReceiveHandler(void *socketInfo)
- *   Description: 向服务器发送初始消息，从服务器循环接收信息.
- *   Called By: server.c[main]
- *   Input: socketInfo -> 表示客户端的网络信息
- *********************************************************************/
-void *fun_thrReceiveHandler(void *socketInfo)
-{
-    int buffer_length;
-    int con;
-    int i;
-    _MySocketInfo _socketInfo = *((_MySocketInfo *)socketInfo);
-
-    /* 向服务器发送握手消息 */
-    char str[80] = HANDSHARK_MSG;
-    char socketCon[25];
-    sprintf(socketCon, "%d", _socketInfo.socketCon);
-    strcat(str, socketCon);
-    send(_socketInfo.socketCon, str, sizeof(str), 0);
-    // _send_msg2allclient(_socketInfo.socketCon);
-
-    /* 从服务器循环接收消息 */
-    while (1)
-    {
-
-        // 将接收缓冲区buffer清空
-        bzero(&buffer, sizeof(buffer));
-
-        // 接收服务器信息
-        printf("Receiving messages from client %d ...\n", _socketInfo.socketCon);
-        buffer_length = recv(_socketInfo.socketCon, buffer, BUFSIZ, 0);
-        if (buffer_length == 0)
-        {
-            // 判断为客户端退出
-            printf("%s:%d Closed!\n", _socketInfo.ipaddr, _socketInfo.port);
-            // 找到该客户端在数组中的位置
-            for (con = 0; con < conClientCount; con++)
-            {
-                if (arrConSocket[con].socketCon == _socketInfo.socketCon)
-                {
-                    break;
-                }
-            }
-            // 将该客户端的信息删除，重置客户端数组
-            for (i = con; i < conClientCount - 1; i++)
-            {
-                arrConSocket[i] = arrConSocket[i + 1];
-            }
-            conClientCount--;
-            break;
-        }
-        else if (buffer_length < 0)
-        {
-            printf("Fail to call read()\n");
-            break;
-        }
-        buffer[buffer_length] = '\0';
-        send_Client = _socketInfo.socketCon;
-        printf("%s:%d said: %s,sockertCon: %d\n", _socketInfo.ipaddr, _socketInfo.port, buffer, send_Client);
-        ReadToSend = 1; // 发送标志置位，允许主线程发送数据
-        usleep(100000);
-    }
-    printf("%s:%d Exit\n", _socketInfo.ipaddr, _socketInfo.port);
-    return NULL;
-}
-
-/********************************************************************
- *   Function Name: checkThrIsKill(pthread_t thr)
- *   Description: 检测当前线程是否存活.
- *   Called By: server.c[main]
- *   Input: thr -> 线程数组中的线程
- *   Date: 2020/05/24
- *********************************************************************/
-int checkThrIsKill(pthread_t thr)
-{
-    int res = 1;
-    int res_kill = pthread_kill(thr, 0);
-    if (res_kill == 0)
-    {
-        res = 0;
-    }
-    return res;
-}
-
-char **_str_split(char *a_str, const char a_delim)
-{
-    char **result = 0;
-    size_t count = 0;
-    char *tmp = a_str;
-    char *last_comma = 0;
-    char delim[2];
-    delim[0] = a_delim;
-    delim[1] = 0;
-
-    /* Count how many elements will be extracted. */
-    while (*tmp)
-    {
-        if (a_delim == *tmp)
-        {
-            count++;
-            last_comma = tmp;
-        }
-        tmp++;
-    }
-
-    /* Add space for trailing token. */
-    count += last_comma < (a_str + strlen(a_str) - 1);
-
-    /* Add space for terminating null string so caller
-       knows where the list of returned strings ends. */
-    count++;
-
-    result = malloc(sizeof(char *) * count);
-
-    if (result)
-    {
-        size_t idx = 0;
-        char *token = strtok(a_str, delim);
-
-        while (token)
-        {
-            assert(idx < count);
-            *(result + idx++) = strdup(token);
-            token = strtok(0, delim);
-        }
-        assert(idx == count - 1);
-        *(result + idx) = 0;
-    }
-
-    return result;
-}
-
-// void _send_msg2allclient(int socketCon)
-// {
-//     char *wecomle="your friend online: ";
-//     char con[5];
-//     sprintf(con,"%d",socketCon);
-//     strcat(wecomle,con);
-//     for (int i = 0; i < conClientCount; i++)
-//     {
-//         int sendMsg_len = send(arrConSocket[i].socketCon,wecomle , strlen(wecomle), 0);
-//         if (sendMsg_len > 0)
-//         {
-//             printf("Send Message to %s:%d successful\n", arrConSocket[i].ipaddr, arrConSocket[i].port);
-//             ReadToSend = 0;
-//         }
-//         else
-//         {
-//             printf("Fail to send message to %s:%d\n", arrConSocket[i].ipaddr, arrConSocket[i].port);
-//         }
-//     }
-// }
